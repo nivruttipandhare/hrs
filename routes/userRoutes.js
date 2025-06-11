40816504fed2f99746623432d29ddcdfc8845180
Sangramk33 <sangramkale4098@gmail.com>
2025-06-11 17:52:07 +0530

    done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,10 +4,16 @@ const router = express.Router();
 // ✅ FIX THIS LINE
 const authController = require('../controllers/authController'); // NOT './authController.js()' or wrong path
 
+router.get('/register', (req, res) => {
+    res.render('register');   // this will render register.ejs
+});
+
+
 router.post('/register', authController.register);
+
 router.post('/login', authController.login);
 
-router.get('/user/dashboard', (req, res) => {
+router.get('/dashboard', (req, res) => {
     if (!req.session.user) {
         return res.redirect('/login');
     }
